43b375b7a7fc8f8fec839ae95b26e5396113e99b
James Cole <james@firefly-iii.org>
2020-02-07 06:31:05 +0100

    Add some sudo calls.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -119,7 +119,7 @@ rm -f $FIREFLY_PATH/storage/logs/laravel.log
 echo "Dump auto load..."
 composer dump-autoload
 echo "Discover packages..."
-php artisan package:discover
+sudo -u www-data php artisan package:discover
 
 echo "Wait for the database."
 if [[ -z "$DB_PORT" ]]; then
@@ -135,15 +135,15 @@ fi
 
 echo "Run various artisan commands..."
 
-php artisan cache:clear
+sudo -u www-data php artisan cache:clear
 
 if [[ $DKR_RUN_MIGRATION == "false" ]]; then
   echo "Will NOT run migration commands."
 else
   echo "Running migration commands..."
-  php artisan firefly-iii:create-database
-  php artisan migrate --seed --no-interaction --force
-  php artisan firefly-iii:decrypt-all
+  sudo -u www-data php artisan firefly-iii:create-database
+  sudo -u www-data php artisan migrate --seed --no-interaction --force
+  sudo -u www-data php artisan firefly-iii:decrypt-all
 fi
 
 # there are 13 upgrade commands
@@ -151,19 +151,19 @@ if [[ $DKR_RUN_UPGRADE == "false" ]]; then
   echo 'Will NOT run upgrade commands.'
 else
   echo 'Running upgrade commands...'
-  php artisan firefly-iii:transaction-identifiers
-  php artisan firefly-iii:migrate-to-groups
-  php artisan firefly-iii:account-currencies
-  php artisan firefly-iii:transfer-currencies
-  php artisan firefly-iii:other-currencies
-  php artisan firefly-iii:migrate-notes
-  php artisan firefly-iii:migrate-attachments
-  php artisan firefly-iii:bills-to-rules
-  php artisan firefly-iii:bl-currency
-  php artisan firefly-iii:cc-liabilities
-  php artisan firefly-iii:back-to-journals
-  php artisan firefly-iii:rename-account-meta
-  php artisan firefly-iii:migrate-recurrence-meta
+  sudo -u www-data php artisan firefly-iii:transaction-identifiers
+  sudo -u www-data php artisan firefly-iii:migrate-to-groups
+  sudo -u www-data php artisan firefly-iii:account-currencies
+  sudo -u www-data php artisan firefly-iii:transfer-currencies
+  sudo -u www-data php artisan firefly-iii:other-currencies
+  sudo -u www-data php artisan firefly-iii:migrate-notes
+  sudo -u www-data php artisan firefly-iii:migrate-attachments
+  sudo -u www-data php artisan firefly-iii:bills-to-rules
+  sudo -u www-data php artisan firefly-iii:bl-currency
+  sudo -u www-data php artisan firefly-iii:cc-liabilities
+  sudo -u www-data php artisan firefly-iii:back-to-journals
+  sudo -u www-data php artisan firefly-iii:rename-account-meta
+  sudo -u www-data php artisan firefly-iii:migrate-recurrence-meta
 fi
 
 # there are 15 verify commands
@@ -171,22 +171,22 @@ if [[ $DKR_RUN_VERIFY == "false" ]]; then
   echo 'Will NOT run verification commands.'
 else
   echo 'Running verification commands...'
-  php artisan firefly-iii:fix-piggies
-  php artisan firefly-iii:create-link-types
-  php artisan firefly-iii:create-access-tokens
-  php artisan firefly-iii:remove-bills
-  php artisan firefly-iii:enable-currencies
-  php artisan firefly-iii:fix-transfer-budgets
-  php artisan firefly-iii:fix-uneven-amount
-  php artisan firefly-iii:delete-zero-amount
-  php artisan firefly-iii:delete-orphaned-transactions
-  php artisan firefly-iii:delete-empty-journals
-  php artisan firefly-iii:delete-empty-groups
-  php artisan firefly-iii:fix-account-types
-  php artisan firefly-iii:rename-meta-fields
-  php artisan firefly-iii:fix-ob-currencies
-  php artisan firefly-iii:fix-long-descriptions
-  php artisan firefly-iii:fix-recurring-transactions
+  sudo -u www-data php artisan firefly-iii:fix-piggies
+  sudo -u www-data php artisan firefly-iii:create-link-types
+  sudo -u www-data php artisan firefly-iii:create-access-tokens
+  sudo -u www-data php artisan firefly-iii:remove-bills
+  sudo -u www-data php artisan firefly-iii:enable-currencies
+  sudo -u www-data php artisan firefly-iii:fix-transfer-budgets
+  sudo -u www-data php artisan firefly-iii:fix-uneven-amount
+  sudo -u www-data php artisan firefly-iii:delete-zero-amount
+  sudo -u www-data php artisan firefly-iii:delete-orphaned-transactions
+  sudo -u www-data php artisan firefly-iii:delete-empty-journals
+  sudo -u www-data php artisan firefly-iii:delete-empty-groups
+  sudo -u www-data php artisan firefly-iii:fix-account-types
+  sudo -u www-data php artisan firefly-iii:rename-meta-fields
+  sudo -u www-data php artisan firefly-iii:fix-ob-currencies
+  sudo -u www-data php artisan firefly-iii:fix-long-descriptions
+  sudo -u www-data php artisan firefly-iii:fix-recurring-transactions
 fi
 
 # report commands
@@ -194,8 +194,8 @@ if [[ $DKR_RUN_REPORT == "false" ]]; then
   echo 'Will NOT run report commands.'
 else
   echo 'Running report commands...'
-  php artisan firefly-iii:report-empty-objects
-  php artisan firefly-iii:report-sum
+  sudo -u www-data php artisan firefly-iii:report-empty-objects
+  sudo -u www-data php artisan firefly-iii:report-sum
 fi
 
 
@@ -205,18 +205,18 @@ if [[ $DKR_RUN_PASSPORT_INSTALL == "false" ]]; then
   echo 'Will NOT generate new OAuth keys.'
 else
   echo 'Generating new OAuth keys...'
-  php artisan passport:install
+  sudo -u www-data php artisan passport:install
 fi
 
-php artisan firefly-iii:set-latest-version --james-is-cool
-php artisan cache:clear
-php artisan config:cache
+sudo -u www-data php artisan firefly-iii:set-latest-version --james-is-cool
+sudo -u www-data php artisan cache:clear
+sudo -u www-data php artisan config:cache
 
-# make sure we own everything
+# make sure we own everything (again)
 echo "Run chown on ${FIREFLY_PATH}/storage"
 chown -R www-data:www-data -R $FIREFLY_PATH/storage
 
-php artisan firefly:instructions install
+sudo -u www-data php artisan firefly:instructions install
 
 echo "Go!"
 exec apache2-foreground
