190636f05f6a24245c02dc0412ea1510853173e8
James Cole <thegrumpydictator@gmail.com>
2020-07-02 21:01:14 +0200

    Show apache version.

--- scripts/prep-image.sh
+++ scripts/prep-image.sh
@@ -3,8 +3,11 @@
 # also room for more commands in the future.
 
 echo "Now in prep-image.sh for Firefly III"
-echo "Script version is 1.0.0 (2020-06-07)"
+echo "Script version is 1.0.1 (2020-07-02)"
 echo "Running as $(whoami)."
+
+echo "Apache version"
+apache2 -v
 
 echo "Making directories..."
 mkdir -p $FIREFLY_PATH
