712f815addb64daf177a43aff5198ce73fa76ddb
James Cole <james@firefly-iii.org>
2020-12-17 15:38:09 +0100

    Build AMD and ARM only.

--- build-travis.sh
+++ build-travis.sh
@@ -7,7 +7,10 @@
 REPOS_NAME=jc5x/firefly-iii
 
 # linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6
-PLATFORMS=linux/amd64,linux/arm64,linux/arm/v7,linux/386,linux/arm/v6
+#PLATFORMS=linux/amd64,linux/arm64,linux/arm/v7,linux/386,linux/arm/v6
+PLATFORMS=linux/amd64,linux/arm64
+
+#,linux/arm/v7,linux/386,linux/arm/v6
 
 #
 # Step 2: echo some info
