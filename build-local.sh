6ae27b6552422d42f4dd90de52ab0abac1583691
James Cole <james@firefly-iii.org>
2020-12-22 17:56:18 +0100

    Build local for all.

--- build-local.sh
+++ build-local.sh
@@ -7,14 +7,12 @@
 REPOS_NAME=jc5x/firefly-iii
 
 # ,linux/arm64,linux/arm/v7,linux/arm/v6
-PLATFORMS=linux/amd64
+PLATFORMS=linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6,linux/386
 
 #
 # Step 2: echo some info
 #
 echo "build-local.sh v1.1: I am building '${VERSION}' for ${REPOS_NAME}."	
-
-
 
 # new script start
 
